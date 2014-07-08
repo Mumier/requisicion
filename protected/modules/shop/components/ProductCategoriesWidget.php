c99bfccbddd016b5406118af311b7e0f5471a160
Mumier <jamumi14@gmail.com>
2014-07-08 15:09:39 -0500

    req1

---
+++ protected/modules/shop/components/ProductCategoriesWidget.php
@@ -0,0 +1,16 @@
+<?php
+Yii::import('zii.widgets.CPortlet');
+
+class ProductCategoriesWidget extends CPortlet {
+
+	public function init() {
+		$this->title = Shop::t('Categorias');
+		return parent::init();
+	}
+
+	public function run() {
+		$this->render('application.modules.shop.views.category.index');
+		return parent::run();
+	}
+}
+?>
---
+++ protected/modules/shop/views/order/admin.php
@@ -0,0 +1,30 @@
+<?php
+$this->breadcrumbs=array(
+	'Orders'=>array('index'),
+	Yii::t('ShopModule.shop', 'Manage'),
+);
+
+?>
+<?php 
+
+$model = new Order();
+
+$this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'order-grid',
+	'dataProvider'=>$model->search(),
+	'filter'=>$model,
+	'columns'=>array(
+		'order_id',
+		'customer.address.firstname',
+		'customer.address.lastname',
+		array('name' => 'ordering_date',
+			'value' => 'date("M j, Y", $data->ordering_date)'),
+		array(
+			'class'=>'CButtonColumn', 
+			'template' => '{view}',
+			'viewButtonUrl' => 'Yii::app()->createUrl("/shop/order/view",
+			array("id" => $data->order_id))',
+		),
+
+	),
+)); ?>
